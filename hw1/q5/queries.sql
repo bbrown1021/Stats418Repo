43290e5556ff0747dd6ce8998ef70be018fe6696
Britney Brown <bbrown1021@g.ucla.edu>
2022-02-08 13:52:22 -0800

    add sql file to answer four queries

--- hw1/q5/queries.sql
+++ hw1/q5/queries.sql
@@ -2,7 +2,6 @@
 ######### Settings ########
 ###########################
 .header ON
-.timer ON
 .mode column 
 
 ###########################
@@ -11,56 +10,75 @@
 .open ../db.sqlite3
 .databases
 .tables
+.schema stores
 .schema sales
 
--- select * from sales
--- limit 100;
-
--- SELECT  saleDate, 
---         (substr(saleDate,4,2) || '-' || substr(saleDate,7,4) || '-' || substr(saleDate,1,2)) as dateconverted,
---         strftime('%Y', (substr(saleDate,7,4) || '-' || substr(saleDate,4,2) || '-' || substr(saleDate,1,2)) ) as year
--- FROM sales
--- LIMIT 10;
-
-SELECT saleDate,strftime('%Y',saleDate) as "Year",
-strftime('%m',saleDate) as "Month",
-strftime('%d',saleDate) as "Day"
-FROM sales
-LIMIT 10;
-
--- select distinct YEAR(saleDate)-- count(*)
--- from sales
--- limit 100
-
--- ;
-
 # 5.2	Which store makes the maximum sales on Sundays?
--- select 
--- 	sum(S1) as s1_sales
---     , sum(s2) as s2_sales
---     , sum(s3) as s3_sales
---     , sum(s4) as s4_sales
---     , sum(s5) as s5_sales
---     , sum(s6) as s6_sales
---     , sum(s7) as s7_sales
---     , sum(s8) as s8_sales
---     , sum(s9) as s9_sales
---     , sum(s10) as s10_sales
--- from sales
--- where DAYOFWEEK(Date) = 1
--- limit 100
--- ;
+SELECT '-------------------------------------';
 
-# 5.3	Find all stores with total sales in December lower than those of S5.
+SELECT name as "Store", sum(sale) as "TotalSales"
+FROM sales
+INNER JOIN stores
+ON stores.storeId = sales.store
+WHERE strftime('%w',saleDate) = '0' -- sunday = 0 day of week
+GROUP BY name
+ORDER BY TotalSales desc
+LIMIT 1
+; 
+
+-- ANSWER: S2
+
+-- # 5.3	Find all stores with total sales in December lower than those of S5.
+SELECT '-------------------------------------';
+
+SELECT name as "Store", sum(sale) as "TotalSales"
+FROM sales
+INNER JOIN stores
+ON stores.storeId = sales.store
+WHERE strftime('%m', saleDate) = '12'
+GROUP BY name
+HAVING sum(sale) < (
+	SELECT sum(sale)
+	FROM sales
+	WHERE store = 5 AND strftime('%m', saleDate) = '12')
+ORDER BY TotalSales
+;
+
+-- ANSWER: S8, S9, S10
 
 # 5.4	Which store recorded the highest number of sales for the largest number of days?
+SELECT '-------------------------------------';
+
+SELECT name, count(name) as 'HighestRecord'
+FROM (
+	SELECT name, saleDate, max(sale)
+	FROM sales
+	INNER JOIN stores
+	ON stores.storeId = sales.store
+	WHERE sale IS NOT NULL
+	GROUP BY saleDate
+	ORDER BY sale ASC
+	)
+GROUP BY name
+ORDER BY HighestRecord DESC
+LIMIT 1
+;
+
+-- ANSWER: S2
+
 
 # 5.5	What week in 2019 has the highest total sales across all the stores?
 
+SELECT strftime('%W',saleDate) as 'Week', sum(sale)
+FROM sales
+INNER JOIN stores
+ON stores.storeId = sales.store
+WHERE strftime('%Y',saleDate) = "2019"
+GROUP BY Week
+ORDER BY sum(sale) DESC
+LIMIT 1
+;
+
+-- ANSWER: 37th week of the year
 
 
--- SELECT DISTINCT saleDate, sale, name FROM sales
--- INNER JOIN stores
--- ON stores.storeId = sales.store
--- WHERE NULLIF(sale, 'N/A') IS NOT NULL
--- AND sale > 80;
